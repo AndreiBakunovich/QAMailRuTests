d0c8eda2002875f122893da613b890b69e3e83d7
Andrei Bakunovich <Andrei_Bakunovich@epam.com>
2018-02-12 19:06:05 +0300

    QA Tests for mail.ru

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Runtime.Remoting.Channels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        public string baseURL;
+
+        private By signIn = By.XPath("//td[@class]//a[text()='Вход']");
+        private By loginField = By.XPath("//span[@class='b-email__name']/input");
+        private By passwordField = By.XPath("//form[@name='login']//*[@name='Password']");
+        private By buttonRememberMeOn = By.XPath("//form[@name='login']//*[@class='js-checkbox b-checkbox b-checkbox_checked b-checkbox_' and contains(@data-bem, 'checkbox')]");
+        private By buttonRememberMeOff = By.XPath("//form[@name='login']//*[@class='js-checkbox b-checkbox b-checkbox_' and contains(@data-bem, 'checkbox')]");
+        private By buttonLogIn = By.XPath("//form[@name='login']//button/span[contains(text(), 'Войти')]");
+        private By textErrorLogIn = By.XPath("//div[contains(text(),'Неверное имя пользователя или пароль. Проверьте правильность введенных данных.')]");
+        private By loginFrame = By.CssSelector("iframe[class='ag-popup__frame__layout__iframe']");
+
+        private string configValue = ConfigurationSettings.AppSettings["BrowserC"];
+
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            if ("FireFox".Equals(this.configValue))
+            {
+                var service = FirefoxDriverService.CreateDefaultService();
+                this.driver = new FirefoxDriver(service);
+            }
+            else
+            {
+                if ("Chrome".Equals(this.configValue))
+                {
+                    ChromeOptions option = new ChromeOptions();
+                    option.AddArgument("disable-infobars");
+                    this.driver = new ChromeDriver(option);
+                }
+            }
+
+            this.baseURL = "https://mail.ru";
+
+            this.driver.Navigate().GoToUrl(this.baseURL);
+            this.driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(6);
+            this.driver.FindElement(signIn).Click();
+            //this.driver.SwitchTo().Frame(this.driver.FindElement(loginFrame));
+            //this.driver.SwitchTo().Frame(1);
+
+            //var frame = this.driver.SwitchTo().Frame(0);
+            this.driver.SwitchTo().Frame(1);
+
+
+            IsElementVisible(loginField);
+            //JavaScriptClick(this.driver.FindElement(signIn));
+            //IAlert simpleAlert = driver.SwitchTo().Alert();
+
+            this.driver.FindElement(loginField).SendKeys("FakeLogin");
+            this.driver.FindElement(passwordField).SendKeys("FakePassword");
+
+            /*if (this.driver.FindElements(buttonRememberMeOn).Count > 0)
+            {
+                this.driver.FindElement(buttonRememberMeOn).Click();
+            }*/
+
+            this.driver.FindElement(buttonLogIn).Click();
+            //JavaScriptClick(driver.FindElement(buttonLogIn));
+            Assert.IsTrue(driver.FindElements(textErrorLogIn).Count > 0);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            this.driver.Close();
+            this.driver.Quit();
+        }
+
+        public void IsElementVisible(By element, int timeoutSecs = 3)
+        {
+            new WebDriverWait(this.driver, TimeSpan.FromSeconds(timeoutSecs)).Until(ExpectedConditions.ElementIsVisible(element));
+        }
+
+        public void JavaScriptClick(IWebElement element)
+        {
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].click();", element);
+        }
+
+    }
+}
