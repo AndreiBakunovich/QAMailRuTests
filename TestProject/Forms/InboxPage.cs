7a9efe8e60660a16efa2598d7f55e1c6568c208b
AndreiBakunovich <bakunovich.andrei@mail.ru>
2018-02-24 01:20:34 +0300

    implement inboxpage

--- TestProject/Forms/InboxPage.cs
+++ TestProject/Forms/InboxPage.cs
@@ -3,17 +3,22 @@ using TestProject.driver;
 
 namespace TestProject.Forms
 {
-    public class Inbox : BaseForm
+    public class InboxPage : BaseForm
     {
         private static readonly By homeLabel = By.XPath("//div[contains(@class,'letters_from')]");
 
-        private readonly By sentButtonLocator = By.XPath("//*[contains(@href,'sent')]");
-        private readonly By drafButtontsLocator = By.XPath("//*[contains(@href,'drafts')]");
-        private readonly By spamButtonLocator = By.XPath("//*[contains(@href,'spam')]");
-        private readonly By trashButtonLocator = By.XPath("//*[contains(@href,'trash')]");
+        private readonly By sentMailsLocator = By.XPath("//*[contains(@href,'sent')]");
+        private readonly By drafMailstsLocator = By.XPath("//*[contains(@href,'drafts')]");
+        private readonly By spamMailsLocator = By.XPath("//*[contains(@href,'spam')]");
+        private readonly By trashMailsLocator = By.XPath("//*[contains(@href,'trash')]");
 
-        protected Inbox() : base(homeLabel, "Inbox")
+        protected InboxPage() : base(homeLabel, "Inbox")
         {
+        }
+
+        public void ClickSentMailButton()
+        {
+            this.
         }
     }
 }
