e3a8f288cc340da4df2860e88fa86600bdbf7930
AndreiBakunovich <bakunovich.andrei@mail.ru>
2018-02-24 00:55:07 +0300

    create trashbox and rename inbox and inboxpage

---
+++ TestProject/Forms/InboxPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using TestProject.driver;
+
+namespace TestProject.Forms
+{
+    public class Inbox : BaseForm
+    {
+        private static readonly By homeLabel = By.XPath("//div[contains(@class,'letters_from')]");
+
+        private readonly By sentButtonLocator = By.XPath("//*[contains(@href,'sent')]");
+        private readonly By drafButtontsLocator = By.XPath("//*[contains(@href,'drafts')]");
+        private readonly By spamButtonLocator = By.XPath("//*[contains(@href,'spam')]");
+        private readonly By trashButtonLocator = By.XPath("//*[contains(@href,'trash')]");
+
+        protected Inbox() : base(homeLabel, "Inbox")
+        {
+        }
+    }
+}
